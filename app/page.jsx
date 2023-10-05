6dc6bbfc90cbdaac30c706e4f9a3f9f969cade48
hithacker <hithacker@gmail.com>
2023-10-05 16:54:20 +0530

    prettier

--- app/page.jsx
+++ app/page.jsx
@@ -1,32 +1,32 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import Head from 'next/head'
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-            <Head>
-                <title>UI Libraries Test</title>
-                <meta name="description" content="Test all UI libraries" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <Head>
+        <title>UI Libraries Test</title>
+        <meta name="description" content="Test all UI libraries" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <main className="p-4 bg-white rounded shadow-md">
-                <h1 className="text-2xl font-bold mb-4">UI Libraries Test</h1>
-                <ul className="space-y-2">
-                    <li>
-                        <Link href="/headless" className="text-blue-500 hover:underline">
-                            Test Headless UI
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/radix" className="text-blue-500 hover:underline">
-                            Test Radix UI
-                        </Link>
-                    </li>
-                </ul>
-            </main>
-        </div>
-    );
+      <main className="p-4 bg-white rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-4">UI Libraries Test</h1>
+        <ul className="space-y-2">
+          <li>
+            <Link href="/headless" className="text-blue-500 hover:underline">
+              Test Headless UI
+            </Link>
+          </li>
+          <li>
+            <Link href="/radix" className="text-blue-500 hover:underline">
+              Test Radix UI
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
